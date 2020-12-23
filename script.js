0647fa99d0d06cf6f180111f5284501962ae73ae
Mihaela <mihaelavalac@yahoo.com>
2020-12-23 18:12:39 -0500

    linked the script file and added comments to it

---
+++ script.js
@@ -0,0 +1,8 @@
+var plannerHours = [ "#nine", "#ten", "#eleven", "#twelve", "#one", "#two", "#three", "#four", "#five"];
+
+// Set the current day information in the day, month and date format.
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+// Change the text area color depending on current time. 
+
+// Save the user input (task) in the local storage and display it on the screen each time when the page is refreshed. 
